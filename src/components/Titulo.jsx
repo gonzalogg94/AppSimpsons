6b9c506bb3ac3443143c2706ca441a19ea469464
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-25 23:42:50 -0300

    agregue imagen principal

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <section className='d-flex'>
+            <img src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd857ffabe0042d12baf2/previews/632cd858ffabe0042d12bb36/download/image.png" alt="" />
+            <button>Obtener brase</button>
+        </section>
+    );
+}
+
+export default Titulo;
