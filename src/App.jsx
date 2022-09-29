116255b0e95ef119fa456cba532587deef674b75
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-29 18:23:32 -0300

    agregamos la consulta a la api

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container,Button } from "react-bootstrap";
 import Frase from './components/Frase';
 import "./style.css";
+import {useEffect,useState} from "react";
 
 function App() {
+  const [personaje, setpersonaje] = useState({});
+
+
+  useEffect(()=>{
+    consultarAPI();
+  },[])
+
+  
+  const consultarAPI=async()=>{
+    try {
+  const respuesta =await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+  console.log(respuesta);
+  const dato= await respuesta.json();
+  console.log(dato[0]);
+  setpersonaje(dato[0]);
+  
+} catch (error) {
+  console.log(error);
+}
+
+  }
+
   return (
   <Container className='py-5'>
     <article className='row text-center pb-5'>
@@ -14,7 +37,7 @@ function App() {
     <Button variant="warning">Obtener Frase</Button>
     </div>
     </article>
-    <Frase></Frase>
+    <Frase personaje={personaje}></Frase>
   </Container>  
   );
 }
