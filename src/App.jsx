ee143dc02766b6214881081c1ad5b0addeac8d97
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-29 17:18:08 -0300

    termine maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from "react-bootstrap";
+import { Container,Button } from "react-bootstrap";
 import Frase from './components/Frase';
-import Titulo from "./components/Titulo";
 import "./style.css";
 
 function App() {
   return (
-  <Container>
-    <Titulo></Titulo>
+  <Container className='py-5'>
+    <article className='row text-center pb-5'>
+    <div className='col-12'>
+      <img className='w-75' src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd857ffabe0042d12baf2/previews/632cd858ffabe0042d12bb36/download/image.png" alt="" />
+    </div>
+    <div className='col-12'>
+    <Button variant="warning">Obtener Frase</Button>
+    </div>
+    </article>
     <Frase></Frase>
   </Container>  
-  
   );
 }
 
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,18 +1,28 @@
-import { Col, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 
 const Frase = () => {
-    return (
-            <Row className="text-center bg-white p-3">
-            <Col>
-<img src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd86034034e0170124eec/previews/632cd86134034e0170124ef2/download/image.png" alt="" />
-            </Col>            
-            <Col className="text-start">
-            <h1>Homer Simpson</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem commodi aut neque deserunt ratione dignissimos molestiae pariatur iusto distinctio fugiat.</p>
-            </Col>
-            </Row>
-        
-    );
+  return (
+    <Card>
+      <Card.Body>
+        <Row className="text-center bg-white p-3">
+          <Col md={4}>
+            <img
+              src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd86034034e0170124eec/previews/632cd86134034e0170124ef2/download/image.png"
+              alt=""
+            />
+          </Col>
+          <Col md={8}>
+            <Card.Title>Homer Simpson</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              commodi aut neque deserunt ratione dignissimos molestiae pariatur
+              iusto distinctio fugiat.
+            </Card.Text>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Frase;
