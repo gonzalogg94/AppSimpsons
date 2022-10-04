0480647ca7517cb3d32939576338477c57824b20
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-04 17:37:46 -0300

    realice cambios en el maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container,Button } from "react-bootstrap";
+import { Container,Button,} from "react-bootstrap";
 import Frase from './components/Frase';
 import "./style.css";
 import {useEffect,useState} from "react";
+import Spinner from './components/Spinner';
+
 
 function App() {
   const [personaje, setpersonaje] = useState({});
+  const [mostrarSpinner , setMostrarSpinner]=useState(true)
 
 
   useEffect(()=>{
@@ -15,17 +18,24 @@ function App() {
   
   const consultarAPI=async()=>{
     try {
+      // mostrar el spinner
+      setMostrarSpinner(true)
   const respuesta =await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
   console.log(respuesta);
   const dato= await respuesta.json();
   console.log(dato[0]);
   setpersonaje(dato[0]);
-  
+  // mostrar el componente frase
+  setMostrarSpinner(false)
 } catch (error) {
   console.log(error);
 }
 
   }
+
+  // condicion logica if,operardor ternario?:
+  // (condicion logica)?(logica a ejecutar si cumplo la condicion logica):(logica a ejecutar si no cumplo la condicion logica)
+  const mostrarComponente=(mostrarSpinner===true)?( <Spinner></Spinner>):( <Frase personaje={personaje}></Frase>)
 
   return (
   <Container className='py-5'>
@@ -37,7 +47,10 @@ function App() {
     <Button variant="warning" onClick={consultarAPI}>Obtener Frase</Button>
     </div>
     </article>
-    <Frase personaje={personaje}></Frase>
+   {
+    mostrarComponente
+   }
+  
   </Container>  
   );
 }
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import "./spinner.css"
+const Spinner = () => {
+  return (
+    <section className="d-flex justify-content-center">
+    <div className="spinner">
+      <div className="double-bounce1"></div>
+      <div className="double-bounce2"></div>
+    </div>
+    </section>
+  );
+};
+
+export default Spinner;
